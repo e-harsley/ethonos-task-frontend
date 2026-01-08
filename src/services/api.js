7ea28a17e3be1df089f697d61b051af334bd60ad
e-harsley <harsleyosasumwen@gmail.com>
2026-01-08 13:07:47 +0100

    merge

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://ethonos-task-backend.onrender.com'
 
 // Create axios instance
 const api = axios.create({
