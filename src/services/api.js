d2f59bbed1165e7fffabba69f12e36b28953ba09
e-harsley <harsleyosasumwen@gmail.com>
2026-01-08 13:28:18 +0100

    merhe

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://ethonos-task-backend.onrender.com'
+const API_BASE_URL = 'https://ethonos-task-backend.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
